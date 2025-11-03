8399136d92490c197c183203c68fd37f28a1f0b6
k1nesi <kolatimakov342@gmail.com>
2025-11-03 21:09:13 +0300

    main

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+char arr[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+              'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o',
+              'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+              'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+              'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+void pass_gen(int size) {
+  srand(time(NULL));
+
+  char *pass = new char[size + 1];
+  for (int i = 0; i < size; i++) {
+    pass[i] = arr[rand() % 60];
+    std::cout << pass[i];
+  }
+  pass[size] = '\0';
+
+  std::cout << std::endl;
+  delete[] pass;
+
+  return;
+}
+
+int main() {
+
+  int pass_size;
+  std::cout << "Введите размер пароля:";
+  std::cin >> pass_size;
+
+  pass_gen(pass_size);
+
+  return 0;
+}
